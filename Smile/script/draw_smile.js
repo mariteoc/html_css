4fe27ea5dc652f3fc07ae0c7b4ce22dc91d2ff45
Mariana <marii.teoc@gmail.com>
2022-12-28 12:26:10 -0800

    update

---
+++ Smile/script/draw_smile.js
@@ -0,0 +1,76 @@
+let choice = document.getElementById("select");
+console.log(choice);
+
+choice.addEventListener('change', function(){
+    let result = choice.value;
+
+    if(result == "smile1"){
+        
+        console.log(smile1);
+        let square = document.querySelectorAll("div[id^='div']");
+        for(let n in square)
+        {
+            square[n].className = ("class","yellow");
+        }
+        for(let i=0;i<smile1.length; i++)
+        {
+            let position;
+            let out = smile1[i];
+            let linePosition = out[0];
+            let columnPosition = out[1];
+            if (linePosition == 0)
+            {               
+                position = linePosition + columnPosition;
+                let square = document.querySelector("div[id^='div"+ position +"']");
+                square.setAttribute("class","black");
+            }
+            else if(linePosition >=1 && linePosition <=11)
+            {
+                linePosition = linePosition * 12;
+                position = linePosition + columnPosition;
+                let square = document.querySelector("div[id^='div"+ position +"']");
+                square.setAttribute("class","black");
+            }
+            else
+            {
+                let square = document.querySelectorAll("div[id^='div']");
+                console.log(square.id);
+            }
+        }
+    }
+    else if(result == "smile2"){
+        
+        console.log(smile2);
+        let square = document.querySelectorAll("div[id^='div']");
+        for(let n in square)
+        {
+            square[n].className = ("class","white");
+        }
+        for(let i=0;i<smile2.length; i++)
+        {
+            let position;
+            let out = smile2[i];
+            let linePosition = out[0];
+            let columnPosition = out[1];
+            console.log(linePosition);
+            if (linePosition == 0)
+            {
+                position = linePosition + columnPosition;
+                console.log(position);
+                let square = document.querySelector("div[id^='div"+ position +"']");
+                console.log(square.id);
+                square.setAttribute("class","blue");
+            }
+            else
+            {
+                linePosition = linePosition * 12;
+                position = linePosition + columnPosition;
+                console.log(position);
+                let square = document.querySelector("div[id^='div"+ position +"']");
+                console.log(square.id);
+                square.setAttribute("class","blue");
+            }
+            
+        }
+    }
+});
